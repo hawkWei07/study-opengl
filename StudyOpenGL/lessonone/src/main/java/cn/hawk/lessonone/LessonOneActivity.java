849bed89e0f137ed8b627f1d696a23f15c0f1c12
hawkWei07 <879421299@qq.com>
2016-07-15 15:29:21 +0800

    [Add] Add module of lesson one for OpenGL study.

---
+++ StudyOpenGL/lessonone/src/main/java/cn/hawk/lessonone/LessonOneActivity.java
@@ -0,0 +1,13 @@
+package cn.hawk.lessonone;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LessonOneActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lesson_one);
+    }
+}
