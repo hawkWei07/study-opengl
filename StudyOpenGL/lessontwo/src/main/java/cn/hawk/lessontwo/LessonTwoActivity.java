a14d5902d4c8ff6b8f49faf985f97b40808932f4
hawkWei07 <879421299@qq.com>
2016-07-19 14:15:38 +0800

    [Add] Add the second lesson.

---
+++ StudyOpenGL/lessontwo/src/main/java/cn/hawk/lessontwo/LessonTwoActivity.java
@@ -0,0 +1,13 @@
+package cn.hawk.lessontwo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LessonTwoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lesson_two);
+    }
+}
