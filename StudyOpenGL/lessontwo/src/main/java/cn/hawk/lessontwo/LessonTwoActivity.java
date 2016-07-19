cef530ecb288a79d0ba0d9fc361f1771ef693acc
hawkWei07 <879421299@qq.com>
2016-07-19 16:27:20 +0800

    [Add] Complete the second lesson.

--- StudyOpenGL/lessontwo/src/main/java/cn/hawk/lessontwo/LessonTwoActivity.java
+++ StudyOpenGL/lessontwo/src/main/java/cn/hawk/lessontwo/LessonTwoActivity.java
@@ -1,13 +1,44 @@
 package cn.hawk.lessontwo;
 
+import android.app.ActivityManager;
+import android.content.Context;
+import android.content.pm.ConfigurationInfo;
+import android.opengl.GLSurfaceView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class LessonTwoActivity extends AppCompatActivity {
 
+    private GLSurfaceView mGLSurfaceView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_lesson_two);
+        mGLSurfaceView = new GLSurfaceView(this);
+
+        // Check if the system supports the OpenGL ES 2.0
+        final ActivityManager activityManager = (ActivityManager) getSystemService(Context.ACTIVITY_SERVICE);
+        final ConfigurationInfo configurationInfo = activityManager.getDeviceConfigurationInfo();
+        final boolean supportsEs2 = configurationInfo.reqGlEsVersion >= 0x20000;
+        if (supportsEs2) {
+            mGLSurfaceView.setEGLContextClientVersion(2);
+            mGLSurfaceView.setRenderer(new LessonTwoRenderer());
+        } else {
+            return;
+        }
+        setContentView(mGLSurfaceView);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mGLSurfaceView.onResume();
+    }
+
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mGLSurfaceView.onPause();
     }
 }
