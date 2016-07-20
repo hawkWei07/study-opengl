e9da347d066546137ab430eac4399e981dab6e91
hawkWei07 <879421299@qq.com>
2016-07-20 10:08:09 +0800

    [Add] Modify the comments and try to understand more.

--- StudyOpenGL/lessontwo/src/main/java/cn/hawk/lessontwo/LessonTwoRenderer.java
+++ StudyOpenGL/lessontwo/src/main/java/cn/hawk/lessontwo/LessonTwoRenderer.java
@@ -64,18 +64,17 @@ public class LessonTwoRenderer implements GLSurfaceView.Renderer {
     private final int mNormalDataSize = 3;
 
     /**
-     * Used to hold a light centered on the origin in model space. We need a 4th coordinate so we can get translations to work when
-     * we multiply this by our transformation matrices.
+     * 用于保存一个在空间原点位置的点光源。我们需要保存一个长度为4的数据来和位移矩阵进行相乘
      */
     private final float[] mLightPosInModelSpace = new float[]{0.0f, 0.0f, 0.0f, 1.0f};
 
     /**
-     * Used to hold the current position of the light in world space (after transformation via model matrix).
+     * 用于保存光源在空间中的位置(经过模型矩阵转换后)
      */
     private final float[] mLightPosInWorldSpace = new float[4];
 
     /**
-     * Used to hold the transformed position of the light in eye space (after transformation via modelview matrix)
+     * 用于保存光源在视角矩阵中的位置 (经过模型、对象矩阵转换后)
      */
     private final float[] mLightPosInEyeSpace = new float[4];
 
@@ -85,12 +84,12 @@ public class LessonTwoRenderer implements GLSurfaceView.Renderer {
     private int mPerVertexProgramHandle;
 
     /**
-     * This is a handle to our light point program.
+     * 点光源执行程序操作代理
      */
     private int mPointProgramHandle;
 
     public LessonTwoRenderer() {
-        // Define points for a cube.
+        // 定义一个立方体参数
 
         // X, Y, Z
         final float[] cubePositionData =
@@ -99,6 +98,8 @@ public class LessonTwoRenderer implements GLSurfaceView.Renderer {
                         // if the points are counter-clockwise we are looking at the "front". If not we are looking at
                         // the back. OpenGL has an optimization where all back-facing triangles are culled, since they
                         // usually represent the backside of an object and aren't visible anyways.
+
+                        //如果我们看到一个三角形,绘制顺序是逆时针的,那么我们看到的是这个三角形的正面。
 
                         // Front face
                         -1.0f, 1.0f, 1.0f,
@@ -150,6 +151,7 @@ public class LessonTwoRenderer implements GLSurfaceView.Renderer {
                 };
 
         // R, G, B, A
+        // 各个面的
         final float[] cubeColorData =
                 {
                         // Front face (red)
@@ -205,6 +207,7 @@ public class LessonTwoRenderer implements GLSurfaceView.Renderer {
         // The normal is used in light calculations and is a vector which points
         // orthogonal to the plane of the surface. For a cube model, the normals
         // should be orthogonal to the points of each face.
+        // 各个面的法线矢量
         final float[] cubeNormalData =
                 {
                         // Front face
@@ -256,7 +259,7 @@ public class LessonTwoRenderer implements GLSurfaceView.Renderer {
                         0.0f, -1.0f, 0.0f
                 };
 
-        // Initialize the buffers.
+        // 加载数据
         mCubePositions = ByteBuffer.allocateDirect(cubePositionData.length * mBytesPerFloat)
                 .order(ByteOrder.nativeOrder()).asFloatBuffer();
         mCubePositions.put(cubePositionData).position(0);
@@ -286,17 +289,22 @@ public class LessonTwoRenderer implements GLSurfaceView.Renderer {
                         + "void main()                    \n"    // The entry point for our vertex shader.
                         + "{                              \n"
                         // Transform the vertex into eye space.
+                        // 获得顶点在视角空间的位置
                         + "   vec3 modelViewVertex = vec3(u_MVMatrix * a_Position);              \n"
                         // Transform the normal's orientation into eye space.
+                        // 获得顶点出在视角空间的法向量
                         + "   vec3 modelViewNormal = vec3(u_MVMatrix * vec4(a_Normal, 0.0));     \n"
                         // Will be used for attenuation.
+                        // 获得光源到点的距离
                         + "   float distance = length(u_LightPos - modelViewVertex);             \n"
                         // Get a lighting direction vector from the light to the vertex.
+                        // 获得光源到点的矢量
                         + "   vec3 lightVector = normalize(u_LightPos - modelViewVertex);        \n"
                         // Calculate the dot product of the light vector and vertex normal. If the normal and light vector are
                         // pointing in the same direction then it will get max illumination.
                         + "   float diffuse = max(dot(modelViewNormal, lightVector), 0.1);       \n"
                         // Attenuate the light based on distance.
+                        // 根据距离计算每个点的亮度
                         + "   diffuse = diffuse * (1.0 / (1.0 + (0.25 * distance * distance)));  \n"
                         // Multiply the color by the illumination level. It will be interpolated across the triangle.
                         + "   v_Color = a_Color * diffuse;                                       \n"
@@ -324,10 +332,11 @@ public class LessonTwoRenderer implements GLSurfaceView.Renderer {
 
     @Override
     public void onSurfaceCreated(GL10 gl10, EGLConfig eglConfig) {
-        // Set the background clear color to black.
+        // 设置背景色为黑色
         GLES20.glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
 
         // Use culling to remove back faces.
+        // 移除物体背面
         GLES20.glEnable(GLES20.GL_CULL_FACE);
 
         // Enable depth testing
@@ -359,9 +368,11 @@ public class LessonTwoRenderer implements GLSurfaceView.Renderer {
         final int vertexShaderHandle = compileShader(GLES20.GL_VERTEX_SHADER, vertexShader);
         final int fragmentShaderHandle = compileShader(GLES20.GL_FRAGMENT_SHADER, fragmentShader);
 
+        //获得顶点操作代理
         mPerVertexProgramHandle = createAndLinkProgram(vertexShaderHandle, fragmentShaderHandle,
                 new String[]{"a_Position", "a_Color", "a_Normal"});
 
+        // 为光源定义简单的点操作器
         // Define a simple shader program for our point.
         final String pointVertexShader =
                 "uniform mat4 u_MVPMatrix;      \n"
@@ -370,7 +381,7 @@ public class LessonTwoRenderer implements GLSurfaceView.Renderer {
                         + "{                              \n"
                         + "   gl_Position = u_MVPMatrix   \n"
                         + "               * a_Position;   \n"
-                        + "   gl_PointSize = 5.0;         \n"
+                        + "   gl_PointSize = 15.0;         \n"
                         + "}                              \n";
 
         final String pointFragmentShader =
@@ -414,9 +425,11 @@ public class LessonTwoRenderer implements GLSurfaceView.Renderer {
         float angleInDegrees = (360.0f / 10000.0f) * ((int) time);
 
         // Set our per-vertex lighting program.
+        // 使用顶点渲染程序
         GLES20.glUseProgram(mPerVertexProgramHandle);
 
         // Set program handles for cube drawing.
+        // 获得绘制立方体时需要的参数代理
         mMVPMatrixHandle = GLES20.glGetUniformLocation(mPerVertexProgramHandle, "u_MVPMatrix");
         mMVMatrixHandle = GLES20.glGetUniformLocation(mPerVertexProgramHandle, "u_MVMatrix");
         mLightPosHandle = GLES20.glGetUniformLocation(mPerVertexProgramHandle, "u_LightPos");
@@ -425,6 +438,7 @@ public class LessonTwoRenderer implements GLSurfaceView.Renderer {
         mNormalHandle = GLES20.glGetAttribLocation(mPerVertexProgramHandle, "a_Normal");
 
         // Calculate position of the light. Rotate and then push into the distance.
+        // 已(0,0,2)为原点,绕y轴做半径为5的圆周运动
         Matrix.setIdentityM(mLightModelMatrix, 0);
         Matrix.translateM(mLightModelMatrix, 0, 0.0f, 0.0f, -5.0f);
         Matrix.rotateM(mLightModelMatrix, 0, angleInDegrees, 0.0f, 1.0f, 0.0f);
@@ -460,6 +474,7 @@ public class LessonTwoRenderer implements GLSurfaceView.Renderer {
 
         // Draw a point to indicate the light.
         GLES20.glUseProgram(mPointProgramHandle);
+        // 使用光源渲染程序
         drawLight();
     }
 
@@ -467,21 +482,21 @@ public class LessonTwoRenderer implements GLSurfaceView.Renderer {
      * Draws a cube.
      */
     private void drawCube() {
-        // Pass in the position information
+        // 输入位置信息
         mCubePositions.position(0);
         GLES20.glVertexAttribPointer(mPositionHandle, mPositionDataSize, GLES20.GL_FLOAT, false,
                 0, mCubePositions);
 
         GLES20.glEnableVertexAttribArray(mPositionHandle);
 
-        // Pass in the color information
+        // 输入颜色信息
         mCubeColors.position(0);
         GLES20.glVertexAttribPointer(mColorHandle, mColorDataSize, GLES20.GL_FLOAT, false,
                 0, mCubeColors);
 
         GLES20.glEnableVertexAttribArray(mColorHandle);
 
-        // Pass in the normal information
+        // 输入法线信息
         mCubeNormals.position(0);
         GLES20.glVertexAttribPointer(mNormalHandle, mNormalDataSize, GLES20.GL_FLOAT, false,
                 0, mCubeNormals);
@@ -490,22 +505,28 @@ public class LessonTwoRenderer implements GLSurfaceView.Renderer {
 
         // This multiplies the view matrix by the model matrix, and stores the result in the MVP matrix
         // (which currently contains model * view).
+        // 获得MV矩阵
         Matrix.multiplyMM(mMVPMatrix, 0, mViewMatrix, 0, mModelMatrix, 0);
 
         // Pass in the modelview matrix.
+        // 传入MV矩阵信息
         GLES20.glUniformMatrix4fv(mMVMatrixHandle, 1, false, mMVPMatrix, 0);
 
         // This multiplies the modelview matrix by the projection matrix, and stores the result in the MVP matrix
         // (which now contains model * view * projection).
+        // 获得MVP矩阵
         Matrix.multiplyMM(mMVPMatrix, 0, mProjectionMatrix, 0, mMVPMatrix, 0);
 
         // Pass in the combined matrix.
+        // 传入MVP矩阵信息
         GLES20.glUniformMatrix4fv(mMVPMatrixHandle, 1, false, mMVPMatrix, 0);
 
         // Pass in the light position in eye space.
+        // 传入光源位置信息
         GLES20.glUniform3f(mLightPosHandle, mLightPosInEyeSpace[0], mLightPosInEyeSpace[1], mLightPosInEyeSpace[2]);
 
         // Draw the cube.
+        // 绘制立方体
         GLES20.glDrawArrays(GLES20.GL_TRIANGLES, 0, 36);
     }
 
@@ -517,22 +538,27 @@ public class LessonTwoRenderer implements GLSurfaceView.Renderer {
         final int pointPositionHandle = GLES20.glGetAttribLocation(mPointProgramHandle, "a_Position");
 
         // Pass in the position.
+        // 传入位置信息
         GLES20.glVertexAttrib3f(pointPositionHandle, mLightPosInModelSpace[0], mLightPosInModelSpace[1], mLightPosInModelSpace[2]);
 
         // Since we are not using a buffer object, disable vertex arrays for this attribute.
+        // 使用的不是一个缓存对象,禁用顶点数组功能
         GLES20.glDisableVertexAttribArray(pointPositionHandle);
 
         // Pass in the transformation matrix.
+        // 传入MVP矩阵信息
         Matrix.multiplyMM(mMVPMatrix, 0, mViewMatrix, 0, mLightModelMatrix, 0);
         Matrix.multiplyMM(mMVPMatrix, 0, mProjectionMatrix, 0, mMVPMatrix, 0);
         GLES20.glUniformMatrix4fv(pointMVPMatrixHandle, 1, false, mMVPMatrix, 0);
 
         // Draw the point.
+        // 绘制点光源
         GLES20.glDrawArrays(GLES20.GL_POINTS, 0, 1);
     }
 
     /**
      * Helper function to compile a shader.
+     * 创建渲染器的方法
      *
      * @param shaderType   The shader type.
      * @param shaderSource The shader source code.
@@ -569,6 +595,7 @@ public class LessonTwoRenderer implements GLSurfaceView.Renderer {
 
     /**
      * Helper function to compile and link a program.
+     * 绑定参数的方法
      *
      * @param vertexShaderHandle   An OpenGL handle to an already-compiled vertex shader.
      * @param fragmentShaderHandle An OpenGL handle to an already-compiled fragment shader.
