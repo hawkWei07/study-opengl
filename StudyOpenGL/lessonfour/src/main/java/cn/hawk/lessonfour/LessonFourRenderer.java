905399551f9027091371d048b4633222dd97652f
hawk.wei <hawkvpp@163.com>
2016-08-19 15:21:36 +0800

    [Add] Modify for the lesson 4.

---
+++ StudyOpenGL/lessonfour/src/main/java/cn/hawk/lessonfour/LessonFourRenderer.java
@@ -0,0 +1,32 @@
+package cn.hawk.lessonfour;
+
+import android.opengl.GLSurfaceView;
+
+import javax.microedition.khronos.egl.EGLConfig;
+import javax.microedition.khronos.opengles.GL10;
+
+/**
+ * Created by kehao.wei on 2016/8/19.
+ */
+
+public class LessonFourRenderer implements GLSurfaceView.Renderer {
+    /**
+     * Used for debug logs.
+     */
+    private static final String TAG = "LessonFourRenderer";
+
+    @Override
+    public void onSurfaceCreated(GL10 gl10, EGLConfig eglConfig) {
+
+    }
+
+    @Override
+    public void onSurfaceChanged(GL10 gl10, int i, int i1) {
+
+    }
+
+    @Override
+    public void onDrawFrame(GL10 gl10) {
+
+    }
+}
