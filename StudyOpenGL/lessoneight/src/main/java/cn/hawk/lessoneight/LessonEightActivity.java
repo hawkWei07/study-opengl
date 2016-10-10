0f9de8f0c33b4c694bcf580f322a475997a0886f
hawkWei07 <879421299@qq.com>
2016-10-10 16:25:51 +0800

    [Add] Add Modul lesson eight.

---
+++ StudyOpenGL/lessoneight/src/main/java/cn/hawk/lessoneight/LessonEightActivity.java
@@ -0,0 +1,13 @@
+package cn.hawk.lessoneight;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LessonEightActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lesson_eight);
+    }
+}
